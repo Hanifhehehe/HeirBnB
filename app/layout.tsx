c3b2b7c97107b8b4af585a2cdd45a1463a067e52
hanifhehehe <pramono.hanif@yahoo.com>
2023-05-25 14:20:18 +0700

    Able to display listings

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,9 @@ export default async function RootLayout({
           <RentModal/>
         {/* </ClientOnly> */}
         <Navbar currentUser={currentUser} />
-        {children}
+        <div className='pb-20 pt-28'>
+         {children}
+        </div>
       </body>
     </html>
   )
