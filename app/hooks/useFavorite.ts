014a75372818fb19161d935ec8a058bdb18f1df5
hanifhehehe <pramono.hanif@yahoo.com>
2023-05-26 17:07:13 +0700

    Like unlike functionalities

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,58 @@
+import { useRouter } from "next/navigation";
+import { SafeUser } from "../types";
+import useLoginModal from "./useLoginModal";
+import React, { useCallback, useMemo } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+export default function useFavorite({ listingId, currentUser }: IUseFavorite) {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => axios.delete(`api/favorites/${listingId}`);
+        } else {
+          request = () => axios.post(`api/favorites/${listingId}`);
+        }
+
+        await request();
+        router.refresh();
+        if (hasFavorited) {
+            toast.success("Yippie! you unliked it!");
+        } else {
+            toast.success("Yippie! you liked it!");
+        }
+      } catch (error) {
+        toast.error("Oh no! Something went wrong.");
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
