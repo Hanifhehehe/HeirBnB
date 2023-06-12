dc640ba466ec3272063ca8ab9571753c450d8b24
hanifhehehe <pramono.hanif@yahoo.com>
2023-06-12 17:25:12 +0700

    Finished

---
+++ app/error.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect } from "react"
+import EmptyState from "./components/EmptyState"
+
+interface ErrorStateProps {
+    error: Error
+}
+
+function ErrorState({error} : ErrorStateProps) {
+    useEffect(() => {
+        console.error(error)
+    }, [error])
+    return (
+        <EmptyState
+            title="Uh oh!"
+            subtitle="Something went wrong! Please try again."
+         />
+    )
+}
+
+export default ErrorState
