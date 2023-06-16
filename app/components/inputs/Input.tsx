5319536518144e8e0887e87465f797abe662493a
hanifhehehe <pramono.hanif@yahoo.com>
2023-06-16 20:02:27 +0700

    Added prisma generate to build script, created .env example

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,7 +1,7 @@
-'use client'
-import React from 'react'
-import { FieldValues, UseFormRegister, FieldErrors } from 'react-hook-form';
-import { BiDollar } from 'react-icons/bi';
+"use client";
+import React from "react";
+import { FieldValues, UseFormRegister, FieldErrors } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
   id: string;
@@ -10,7 +10,7 @@ interface InputProps {
   disabled?: boolean;
   formatPrice?: boolean;
   required?: boolean;
-  register: UseFormRegister<FieldValues>,
+  register: UseFormRegister<FieldValues>;
   errors: FieldErrors;
 }
 
@@ -27,12 +27,38 @@ function Input(props: InputProps) {
   } = props;
 
   return (
-    <div className='w-full relative'>
-      {formatPrice && (<BiDollar size='24' className='text-neutral-700 absolute top-5 left-2' />)}
-      <input id={id} disabled={disabled} {...register(id, {required})} placeholder='' type={type} className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed ${formatPrice ? 'pl-9' : 'pl-4'} ${errors[id] ? 'border-pink-700 focus:border-pink-700' : 'border-neutral-300 focus:border-black'}`} />
-      <label className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] ${formatPrice ? 'left-9' : 'left-4'} peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 ${errors[id] ? 'text-pink-700' : 'text-zinc-400'}`}>{label}</label>
+    <div className="w-full relative">
+      {formatPrice && (
+        <BiDollar
+          size="24"
+          className="text-neutral-700 absolute top-5 left-2"
+        />
+      )}
+      <input
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=""
+        type={type}
+        className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed ${
+          formatPrice ? "pl-9" : "pl-4"
+        } ${
+          errors[id]
+            ? "border-pink-700 focus:border-pink-700"
+            : "border-neutral-300 focus:border-black"
+        }`}
+      />
+      <label
+        className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] ${
+          formatPrice ? "left-9" : "left-4"
+        } peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 ${
+          errors[id] ? "text-pink-700" : "text-zinc-400"
+        }`}
+      >
+        {label}
+      </label>
     </div>
   );
 }
 
-export default Input
+export default Input;
